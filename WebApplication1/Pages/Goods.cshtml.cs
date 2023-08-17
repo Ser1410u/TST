82eb819262ef098ddf2f9ad5124ed307bbc83770
СергейПавлов <ser1410u@yandex.ru>
2023-08-17 02:11:31 +0200

    добавил веб версию

---
+++ WebApplication1/Pages/Goods.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class GoodsModel : PageModel
+    {
+        private readonly ILogger<GoodsModel> _logger;
+
+        public GoodsModel(ILogger<GoodsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
